0ead62c6faeaab9893a989b75271f4d8686cce96
Zachary Piontek <zakpiontek@gmail.com>
2022-09-26 14:28:49 -0700

    eslint issue

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
 it('renders a welcome message when signed out', async () => {
+  // eslint-disable-next-line no-unused-vars
   const { container } = render(
     <MemoryRouter>
       <App />
