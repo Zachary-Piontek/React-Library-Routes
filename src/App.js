1c80abc4bbc803c5ca472c47795e3c91c3ae75c4
Zachary Piontek <zakpiontek@gmail.com>
2022-09-26 12:43:46 -0700

    added route for book id and link to return from book

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import BookList from './components/book/BookList';
 import './App.css';
 import { Route } from 'react-router-dom';
+import BookDetail from './components/book/BookDetail.js';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
       <h1>Library Catalog</h1>
+      <Route exact path='/books/:id'>
+        <BookDetail />
+      </Route>
       <Route exact path='/books'>
         <BookList />
       </Route>
